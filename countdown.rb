655fbfcba89234eda218c924c7d819e467b2cbcc
Kat Maldon <katharinamaldon@gmail.com>
2019-03-26 17:14:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,16 @@
 def countdown(seconds)
   while seconds > 0 
     puts "#{seconds} SECOND(S)!"
-    sleep(1.secs)
     seconds -= 1
   end 
   "HAPPY NEW YEAR!"
 end
 
-
+countdown_with_sleep(seconds)
+  while seconds > 0 
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end 
+  "HAPPY NEW YEAR!"
+end
+  
